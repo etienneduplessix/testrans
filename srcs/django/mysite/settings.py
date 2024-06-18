c5c39d78060cdeacf6b6c318a6d583bcbb9cd7b9
Etienne Duplessix <edupless@c3r6s3.42prague.com>
2024-06-18 18:44:14 +0200

    v12888

--- srcs/django/mysite/settings.py
+++ srcs/django/mysite/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
